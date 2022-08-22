9213141840fe6782b257c5a7839922888128a71c
Daniel Williams <danielw@jfrog.com>
2022-08-22 15:25:16 -0600

    Debugging.

--- curate_docker.py
+++ curate_docker.py
@@ -137,6 +137,7 @@ def main():
         # Copy the list.manifest.json to the local repository.
     for tmp_img in tmp_images_v2:
         tmp_layers = tmp_img['manifest']
+        logging.debug("  tmp_layers: %s", tmp_layers)
         for tmp_layer in tmp_layers:
             if tmp_layer['platform']['architecture'] in SUPPORTED_ARCHITECTURES:
                 tmp_layer_name = "__".join(tmp_layer['digest'].split(':'))
